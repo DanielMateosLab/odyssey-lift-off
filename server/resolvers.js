c30ac3c064c950e3901ce71a87fc77b1dfb37846
Daniel Mateos Labrador <daniel.mat.lab@gmail.com>
2022-03-12 11:38:25 +0100

    Create tracksForHome specification

---
+++ server/resolvers.js
@@ -0,0 +1,9 @@
+const resolvers = {
+  Query: {
+    // Returns an array of tracks that will be used to populate
+    // the client homepage grid
+    tracksForHome: (parent, args, context, info) => {},
+  },
+};
+
+module.exports = resolvers;

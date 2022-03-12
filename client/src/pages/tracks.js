69c34a616861b5182ab45cf400ba82d26b1a7f52
Daniel Mateos Labrador <daniel.mat.lab@gmail.com>
2022-03-12 09:54:05 +0100

    Add getTracks query

--- client/src/pages/tracks.js
+++ client/src/pages/tracks.js
@@ -1,5 +1,23 @@
-import React from 'react';
-import { Layout } from '../components';
+import { gql } from "@apollo/client";
+import React from "react";
+import { Layout } from "../components";
+
+export const TRACKS = gql`
+  query getTracks {
+    tracksForHome {
+      id
+      title
+      author {
+        name
+        id
+        photo
+      }
+      thumbnail
+      length
+      modulesCount
+    }
+  }
+`;
 
 /**
  * Tracks Page is the Catstronauts home page.

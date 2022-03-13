1a775be8bddf8b3ecf261f18f419267892d749c1
Daniel Mateos Labrador <daniel.mat.lab@gmail.com>
2022-03-13 09:37:41 +0100

    Update ApolloClient URI

--- client/src/index.js
+++ client/src/index.js
@@ -5,7 +5,7 @@ import Pages from "./pages";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "http://localhost:4000",
+  uri: process.env.API_URI || "http://localhost:4000",
   cache: new InMemoryCache(),
 });
 
